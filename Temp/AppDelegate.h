e7ee2539f710a3873549eb40ca8ff6c69757b1f2
Roland Tecson <roland_tecson@hotmail.com>
2018-04-18 09:05:41 -0700

    Initial Commit

---
+++ Temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Temp
+//
+//  Created by Roland on 2018-04-18.
+//  Copyright © 2018 MoozX Internet Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
